eda3cd82bdc6579ec01cfc9554888511a14a0531
Fidel Elie <felie@tabernacleschool.co.uk>
2021-08-20 01:40:14 +0100

    Added App Utilities

--- src/lib/utiltiies.js
+++ src/lib/utiltiies.js
@@ -4,4 +4,41 @@ const numberArray = (start, stop, step = 1) => {
   );
 }
 
-export { numberArray };
+const bpmToMsPeriod = (bpm) => {
+  // Convert beats per minute to beats per second
+  const beatsPerSecond = bpm / 60;
+  // Convert frequency to a period in seconds
+  const period = 1 / beatsPerSecond;
+  // Convert value to milliseconds
+  const beatInterval = period * 1000;
+
+  return beatInterval;
+}
+
+const msPeriodToBPM = (deltaInMilliseconds, normaliseBPM = 220) => {
+  // deltaInMilliseconds / (milisecondsInASecond * secondsInAMinute)
+  const millisecondsInAMinute = deltaInMilliseconds / (1000 * 60);
+  // beatsPerMinute = 1 minute / beatPerMinuteDelta
+  const beatsPerMinute = Math.ceil(1 / millisecondsInAMinute);
+  // Normalise to a maximum of {nomaliseBPM} BPM
+  const normalised = beatsPerMinute <= normaliseBPM ? beatsPerMinute : normaliseBPM;
+
+  return normalised;
+}
+
+const joinClasses = (...classes) => {
+  const classesArray = classes.map(x => {
+    if (typeof x == "string" || x instanceof String) {
+      return x
+    } else if (x instanceof Array) {
+      return joinClasses(...x);
+    } else if (x instanceof Object) {
+      return Object.entries(x).map(x => x[1] ? x[0] : null).filter(x => x).join(" ");
+    } else {
+      throw new TypeError("Invalid Class Type Provided, Expected: String, Array Or Object");
+    }
+  })
+  return classesArray.join(" ");
+}
+
+export { numberArray, bpmToMsPeriod, msPeriodToBPM, joinClasses };
