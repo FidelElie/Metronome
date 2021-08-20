85ef1540baa655175ec8362545619e35d5f2e252
Fidel Elie <felie@tabernacleschool.co.uk>
2021-08-20 01:39:34 +0100

    Fixed Beat Click Method

--- src/components/pages/index/Click.js
+++ src/components/pages/index/Click.js
@@ -1,45 +1,38 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
+
+// ! Library
+import { msPeriodToBPM } from "../../../lib/utiltiies";
 
 const Click = (props) => {
   const { setMetronomeBPM } = props;
   const [currentDate, setCurrentDate] = useState(null);
-  const calculationRef = useRef(null);
 
-  const startTiming = () => {
-    if (!currentDate) {
-      setCurrentDate(new Date());
-      calculationRef.current = setTimeout(() => setCurrentDate(null), 5000);
-    } else {
-      clearTimeout(calculationRef.current);
-      calculationRef.current = null;
-      const dateAfterCalculation = new Date();
-      const deltaInMilliseconds = dateAfterCalculation.getTime() - currentDate.getTime();
-      // deltaInMilliseconds / (milisecondsInASecond * secondsInAMinute)
-      const millisecondsInAMinute = deltaInMilliseconds / (1000 * 60);
-      // two beats occur in delta
-      const beatPerMinuteDelta = millisecondsInAMinute / 2;
-      // beatsPerMinute = 1 minute / beatPerMinuteDelta
-      const beatsPerMinute = Math.ceil(1 / beatPerMinuteDelta);
-      // Normalise to a maximum of 220 BPM
-      const normalised = beatsPerMinute <= 220 ? beatsPerMinute : 220;
-      setMetronomeBPM(normalised);
-      setCurrentDate(null);
-    }
+  const tapBeat = () => {
+    const dateAfterCalculation = new Date();
+
+    const deltaInMilliseconds = dateAfterCalculation.getTime() - currentDate.getTime();
+    const bpm = msPeriodToBPM(deltaInMilliseconds);
+
+    setMetronomeBPM(bpm);
+    setCurrentDate(dateAfterCalculation);
   }
+
+  useEffect(() => {
+    setCurrentDate(new Date());
+  }, []);
 
   return (
     <div className="flex w-full px-10 box-border justify-center items-center">
       <button
-        className={`border-2 border-transparent rounded-full shadow p-2 h-12 w-12 text-lg flex items-center justify-center bg-gray-500 text-white ${currentDate ? "border-yellow-600" : ""} focus:outline-none mr-5`}
-        onClick={startTiming}>
+        className="border-2 border-transparent rounded-full shadow p-2 h-12 text-2xl flex items-center justify-center bg-gray-500 text-white mr-5 focus:outline-none"
+        onClick={tapBeat}
+      >
         <span className="material-icons md:hidden">touch_app</span>
-        <span className="hidden material-icons md:inline">mouse</span>
-      </button>
-      <div className="flex items-center justify-center">
-        <span className="text-gray-500">
-          { currentDate ? "Click Again For Beat" : "Click To Start Timer"}
+        <span className="hidden material-icons lg:inline">mouse</span>
+        <span className="ml-3 text-sm md:text-base">
+          Tap Beat
         </span>
-      </div>
+      </button>
     </div>
   )
 }
